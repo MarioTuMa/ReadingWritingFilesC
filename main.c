d29e6ff73378a56aca941b6e817c773eadd71d74
Marioisdabest <mtutuncumacias1@gmail.com>
2019-11-10 17:03:38 -0500

    Made exception cases

--- main.c
+++ main.c
@@ -16,20 +16,36 @@ int main(){
   for(i=0;i<10;i++){
     int randNum=0;
     int *buff=&randNum;
-    read(randomDataFile, buff, sizeof(int));
+    if(read(randomDataFile, buff, sizeof(int))<0){
+      printf("%s\n",strerror(errno));
+      return errno;
+    }
     printf("%d\n",randNum);
     randos[i]=randNum;
   }
-  close(randomDataFile);
+  if(close(randomDataFile)<0){
+    printf("%s\n",strerror(errno));
+    return errno;
+  }
+
   int fileToWriteTo = open("fileToWriteTo.txt", O_WRONLY|O_TRUNC);
   if(fileToWriteTo<0){
     fileToWriteTo = open("fileToWriteTo.txt",O_WRONLY|O_CREAT,0640);
   }
-  write(fileToWriteTo, &randos, 10*sizeof(int));
-  close(fileToWriteTo);
+  if(write(fileToWriteTo, &randos, 10*sizeof(int))<0){
+    printf("%s\n",strerror(errno));
+    return errno;
+  }
+  if(close(fileToWriteTo)<0){
+    printf("%s\n",strerror(errno));
+    return errno;
+  }
   int fileToReadFrom = open("fileToWriteTo.txt", O_RDONLY);
   int *randos2;
-  read(fileToReadFrom, randos2, 10*sizeof(int));
+  if(read(fileToReadFrom, randos2, 10*sizeof(int))<0){
+    printf("%s\n",strerror(errno));
+    return errno;
+  }
   for(i=0;i<10;i++){
     printf("Read %d from random file, and %d from file I made\n",randos[i],randos2[i]);
   }
