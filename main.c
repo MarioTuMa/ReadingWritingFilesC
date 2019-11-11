fb5cbd69237c78a53a6bcb43847249ed890ec194
Marioisdabest <mtutuncumacias1@gmail.com>
2019-11-10 20:37:38 -0500

    Fixed seg fault

--- main.c
+++ main.c
@@ -4,36 +4,36 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <errno.h>
-
-int rngesus(int randomDataFile){
-  int randNum=0;
-  int *buff=&randNum;
-  if(read(randomDataFile, buff, sizeof(int))<0){
-    printf("%s\n",strerror(errno));
-  }
-  return randNum;
-}
-
-int main(){
+int randNum(){
   int randomDataFile = open("/dev/random", O_RDONLY);
   if(randomDataFile<0){
     printf("%d\n",randomDataFile);
     printf("%s\n",strerror(errno));
     return randomDataFile;
   }
+  int randNum=0;
+
+  if(read(randomDataFile, &randNum, sizeof(int))<0){
+    printf("%s\n",strerror(errno));
+    return 0;
+  }
+  close(randomDataFile);
+  return randNum;
+}
+
+int main(){
+  printf("hi\n");
   int randos[10];
   int i;
   for(i=0;i<10;i++){
-    randos[i]=rngesus(randomDataFile);
-  }
-  if(close(randomDataFile)<0){
-    printf("%s\n",strerror(errno));
-    return errno;
+    randos[i]=randNum();
+    printf("Generated %d \n",randos[i]);
   }
 
   int fileToWriteTo = open("fileToWriteTo.txt", O_WRONLY|O_TRUNC);
+
   if(fileToWriteTo<0){
-    fileToWriteTo = open("fileToWriteTo.txt",O_WRONLY|O_CREAT,0640);
+    fileToWriteTo = open("fileToWriteTo.txt",O_WRONLY|O_CREAT|O_EXCL,0640);
   }
   if(write(fileToWriteTo, &randos, 10*sizeof(int))<0){
     printf("%s\n",strerror(errno));
@@ -44,15 +44,16 @@ int main(){
     return errno;
   }
   int fileToReadFrom = open("fileToWriteTo.txt", O_RDONLY);
-  int *randos2;
-  if(read(fileToReadFrom, randos2, 10*sizeof(int))<0){
+  int randos2[10];
+
+  if(read(fileToReadFrom, &randos2, 10*sizeof(int))<0){
     printf("%s\n",strerror(errno));
     return errno;
   }
+  //printf("Got here \n");
+
   for(i=0;i<10;i++){
-    printf("%d: ",i);
     printf("Read %d from random file, and %d from file I made\n",randos[i],randos2[i]);
   }
-  //printf("made it here %d \n",errno);
-  return close(fileToReadFrom);
+  return 0;
 }
